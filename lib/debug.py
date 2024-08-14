e3213a5278d9449f5d87083363c5a63af10b8c32
Manny <mhfernan35277@gmail.com>
2024-08-13 21:36:06 -0700

    add file

--- lib/debug.py
+++ lib/debug.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import ipdb
+import random
 
 from classes.many_to_many import NationalPark
 from classes.many_to_many import Visitor
@@ -7,5 +8,24 @@ from classes.many_to_many import Trip
 
 if __name__ == '__main__':
     print("HELLO! :) let's debug :vibing_potato:")
+national_park = NationalPark("Yosemite")
+national_park2 = NationalPark("YellowStone")
+national_park3 = NationalPark("Wolfe") 
 
-    ipdb.set_trace()
+national_park_list = [national_park, national_park2, national_park3]
+
+# national_park.best_visitor = "Victor"
+# ipdb.set_trace()
+visitor = Visitor("Victor")
+visitor2 = Visitor("Denise")
+visitor3 = Visitor("Angel")
+visitor4 = Visitor("Dan")
+
+visitor_list = [visitor, visitor2, visitor3, visitor4]
+
+
+# trip = Trip(visitor, "Yosemite", )
+for i in range(5):
+    Trip(visitor_list[random.randint(0,3)], national_park_list[random.randint(0,2)], "08-13-2024", "08-13-2024")
+
+national_park.best_visitor()
